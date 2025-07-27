b532eb7b6b274f3b3f36dfe617509cf67fab28a6
Fernando Montiel <fernandomonti0329@gmail.com>
2025-07-27 11:42:51 -0500

    Initializing the repository

---
+++ node-first-app/play.js
@@ -0,0 +1,11 @@
+var name = 'fernando';
+var age = 19;
+var hasHobbies = true;
+
+function summarizeUser() {
+    return (
+        'Name is ' + name + ', age is ' + age + ', and user has hobbies: ' + hasHobbies
+    );
+}
+
+console.log(summarizeUser());
