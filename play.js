9eb03dab09b5e96deaa872a2afeebcf639e6589d
Fernando Montiel <fernandomonti0329@gmail.com>
2025-07-27 19:49:02 -0500

    New features into the play.js file

--- play.js
+++ play.js
@@ -9,3 +9,13 @@ const summarizeUser = () => {
 }
 
 console.log(summarizeUser());
+
+const hobbies = ['Sports', 'Cooking'];
+
+const copiedHobbies = [...hobbies];
+console.log(copiedHobbies);
+
+const toArray = (...args) => {
+    return args;
+}
+console.log(toArray(1, 2, 3, 4, 5));
