1968f956b13fdf00e1997fb2868b1709636cf7ee
Fernando Montiel <fernandomonti0329@gmail.com>
2025-07-29 08:37:42 -0500

    Added new features

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -12,7 +13,7 @@ app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 router.post('/product', (req, res ,next) => {
     console.log(req.body);
