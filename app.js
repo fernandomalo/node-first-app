17eaba28f82671c9e009094d93c7f80c7b0ac3c4
Fernando Montiel <fernandomonti0329@gmail.com>
2025-07-29 07:37:23 -0500

    Changes into the app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const http = require('http');
 const express = require('express');
+
 const app = express();
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
 
 app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
-});
-app.use((req, res, next) => {
-    console.log('In another middleware');
+    res.status(404).send('<h1>Page not found</h1>');
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
