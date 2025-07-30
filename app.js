091046a4df3405d409a72034f565365a1e96986f
Fernando Montiel <fernandomonti0329@gmail.com>
2025-07-30 05:43:40 -0500

    Configure the app to use pug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/admin', adminData.router);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    res.status(404).render('not-found', { pageTitle: 'Page Not Found' });
 })
 
 app.listen(3000);
