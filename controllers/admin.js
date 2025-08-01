571f86a3fe87c04ac02f6fa3069d8b8c73dafc97
Fernando Montiel <fernandomonti0329@gmail.com>
2025-08-01 06:05:13 -0500

    Using query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {pageTitle: 'add product', path: '/admin/add-product'});
+    res.render('admin/edit-product', {
+        pageTitle: 'add product', 
+        path: '/admin/add-product',
+        editing: false
+    });
 }
 
 exports.postAddProduct = (req, res ,next) => {
@@ -12,6 +16,30 @@ exports.postAddProduct = (req, res ,next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit product', 
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    })
+}
+
+exports.postEditProduct = (req, res, next) => {
+    
 }
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,13 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
+
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/edit-product'); 
 
 module.exports = router;
