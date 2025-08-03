1fbf3127c7a302ccd6055e4dadbfe1e757d5981f
Fernando Montiel <fernandomonti0329@gmail.com>
2025-08-03 14:12:40 -0500

    Adding a cart model and its relations

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const adminRoutes = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const notFoundController = require('./controllers/notFound');
 
@@ -36,6 +38,10 @@ app.use(notFoundController.get404Error);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize.sync()
     .then(result => {
