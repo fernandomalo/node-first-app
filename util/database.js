9ba7f1bdd835b5a4f6cd358f97cb168adebdf5af
Fernando Montiel <fernandomonti0329@gmail.com>
2025-08-04 10:46:17 -0500

    Creating the database connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '1234', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://fernando:fernando.29@sandbox.sjuqetg.mongodb.net/?retryWrites=true&w=majority&appName=Sandbox')
+    .then(client => {
+        console.log('connected!');
+        callback(client);
+    })
+    .catch(err => console.log(err))
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
