a9d41ce5853f5e41f02d17f81206e035e945d400
Fernando Montiel <fernandomonti0329@gmail.com>
2025-08-07 09:58:06 -0500

    Created the update and delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,7 +37,7 @@ exports.getEditProduct = (req, res, next) => {
     }
 
     const prodId = req.params.productId;
-    Product.findByPk(prodId)
+    Product.findById(prodId)
     //Product.findByPk(prodId)
         .then(product => {
             if (!product) {
@@ -60,8 +60,14 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
 
-    const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, prodId);
-    product.save()
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle,
+            product.price = updatedPrice,
+            product.description = updatedDescription,
+            product.imageUrl = updatedImageUrl
+            return product.save();
+        })
         .then(result => {
             console.log('Updated Product');
             res.redirect('/admin/products');
@@ -71,7 +77,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    Product.findByIdAndDelete(prodId)
         .then(result => {
             console.log('Deleted Product');
             res.redirect('/admin/products');
@@ -80,7 +86,7 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
