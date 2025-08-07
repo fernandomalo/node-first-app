bff78dcc1cc39fa81a28bf631a3920fe2c2451df
Fernando Montiel <fernandomonti0329@gmail.com>
2025-08-07 10:18:57 -0500

    Creating a user model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRouter = require('./routes/shop');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const notFoundController = require('./controllers/notFound');
 
@@ -21,14 +21,14 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findByPk('68932f15d4ca73d7b862de6c')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('6894c22047b7b3e1d6526aa2')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRouter);
@@ -37,6 +37,19 @@ app.use(notFoundController.get404Error);
 
 mongoose.connect('mongodb+srv://fernando:fernando.29@sandbox.sjuqetg.mongodb.net/shop?retryWrites=true&w=majority&appName=Sandbox')
     .then(result => {
+        User.findOne()
+            .then(user => {
+                if (!user) {
+                    const user = new User({
+                        name: 'Fernando',
+                        email: 'fernando@example.com',
+                        cart: {
+                            items: []
+                        }
+                    });
+                    user.save();
+                }
+            })
         app.listen(3000);
     })
     .catch(err => console.log(err));
